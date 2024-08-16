f1681f5c533fac8675923e897ee5e3c60ab6667e
JunYeob <105794285+dev-0eum@users.noreply.github.com>
2024-08-16 14:22:31 +0000

    10828 Stack.java [Challenging]

---
+++ Files/Stack.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Stack{
+    public static void main(String[] args) throws Exception{
+        
+        // Get params
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        str = br.readLine();
+
+        int cnt = 0;
+        cnt = Integer.parseInt(str);
+        
+        int size = 0;
+        int top = -1;
+        int[] arr = new int[5];
+
+        System.out.println(cnt);
+
+        for(int i=0;i<cnt;i++){
+            
+        }
+    }
+
+}
