9de9c067b144a3f295490f0e7a33ac8d5469bd28
JunYeob <105794285+dev-0eum@users.noreply.github.com>
2024-08-17 02:00:06 +0000

    10828 Stack.java

--- Files/Stack.java
+++ Files/Stack.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Stack{
     public static void main(String[] args) throws Exception{
@@ -13,12 +14,46 @@ public class Stack{
         
         int size = 0;
         int top = -1;
-        int[] arr = new int[5];
-
-        System.out.println(cnt);
+        ArrayList<Integer> arr = new ArrayList<>();
 
         for(int i=0;i<cnt;i++){
+            String q = "";
+            q = br.readLine();
+            // System.out.println(i+":"+q);
+
+            String[] cmd = q.split(" ");
             
+            if(cmd[0].equals("push")){
+                int tmp = Integer.parseInt(cmd[1]);
+                arr.add(tmp);
+                top++; 
+                size++;
+            }
+            else if(cmd[0].equals("pop")){ // print top & del
+                if(size < 1)
+                    System.out.println(-1);
+                else{
+                    System.out.println(arr.get(top));
+                    arr.remove(top--);
+                    size--;
+                }
+            }
+            else if(cmd[0].equals("top")){ // print
+                if(size < 1)
+                    System.out.println(-1);
+                else
+                    System.out.println(arr.get(top));
+            }
+            else if(cmd[0].equals("empty")){
+                if(size==0)
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            }
+            else if(cmd[0].equals("size")){
+                System.out.println(size);
+            }
+            // System.out.println("top: "+top);
         }
     }
 
